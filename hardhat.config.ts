eb3d07353fb75ebd2ca4ff1e22127ba40709faa1
Soumikm1 <soumikkvb@gmail.com>
2024-06-02 23:04:36 +0530

    Still tring to make it Besu-compatible !!

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,49 +1,34 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-require("@nomiclabs/hardhat-ganache");
 require("@nomiclabs/hardhat-waffle");
-// import "@nomiclabs/hardhat-web3";
 
-// const config: HardhatUserConfig = {
-//   solidity: {
-//     version: "0.8.0",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 5,
-//       },
-//     },
-//   },
-// };
+const accounts = [
+  "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113b841ba9335e0f0f4c349e17a",
+  "0x6c8f795b4b1e3b04c290089c4e4f90b4f92e4f6f2767b4cf451759b021f7dc50",
+  "0x6370fd0333e1e4c9a0d7b2e4b1da8f6c1c78d6f019a1f34303a9a5d03cb23f19",
+  "0x646f1ce2d8f07dcb92e6b2c73f687ac3b3ffb4183b6e8ac8d2b0c781326bedd8",
+  "0xadd53f9a7e588d08b7e4b8ff2318970cba7245a2aefdd9f0df43b6a5f65242b1",
+  "0x395df67f0c2eebd8cfadee3823d1d88992ef09ddbf1efab48d4c5c8315701a70",
+  "0x5a75cb7b8c3c10294a146fc99e370ff50fdd36e2ffb8b2e5e6e6e29146fecc5b",
+  "0xf5801fb8c92037bcd0d2a21f8c91f8d75b9f0c8c2456d4c7f8285a2d55f9f1d1",
+  "0x8b4bfc7a3f201317d7e430bf6f2c69a36208d0ad8356ffb7f15e5d2f2fdf133b",
+  "0x5a9d4380e08ff443b97fdc6f75fbcdbe319042f824be1f69ad5d74d9fa64c11e",
+  "0xb94f5374fce5edbc8e2a8697c15331677e6ebf0b4b55fc5478f52a2d3c4a56a0",
+  "0x527dcfbf7b1f5b054e7c32b79d8fa48a9f124a6bca1b18a8f7430a5a6e27b4e5",
+  "0x7307baf35c60e6a39ec3e158b6b8c3ecf1c6f43dc9af27fa116bb1ff0d44a2d7",
+  "0x32d8c6a7c97fb5d4f1651ffac9b741d05efb37ab87c1d59b463c8f8b2b7b1ba1",
+  "0x218f56b3f7c79913f7a63a59c06d3d3136841c2b4e89c0d0480f13c8e938b60d",
+  "0x341c7adf507dfba1839b7178fbfe6f5f0d2293c9e7b4d97f7611c5f3c96d7f6e",
+  "0xbda5747bfd65f08deb54cb465eb87d40e51b197e5b6f645c43db4ce6e8b3db42",
+  "0x56e6b7a58dcf3b2e6fbe7fb5bb63467b6b2a2b6b9c60db7b1c73c3f77e93b8cf",
+  "0x9214f5b1bdb6de3c9a252b2aa4d18acbe16cfbb9cf028360a50fa82e845a1677",
+  "0x5a6deaa2bc413fb3a5cfdf6f0e5a8a25a6f37d07b7e79d648fc3ecbff245d916"
+];
+
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.4",
   networks: {
     besu: {
       url: "http://127.0.0.1:8545",
-      accounts: [
-        "0x627306090abab3a6e1400e9345bc60c78a8bef57",  // Private key for account 1
-        "0xf17f52151ebef6c7334fad080c5704d77216b732",  // Private key for account 2
-        "0xc5d2460186f7233c927e7db2dcc703c0e500db34",  // Private key for account 3
-        "0x92d8f1024e6d77597e92ef4dc727587cc38a4472",  // Private key for account 4
-        "0x5aeda56215b167893e80b4fe645ba6d5bab767de",  // Private key for account 5
-        "0x3a1c41cf80c4f09f9b33b914aab6aeb26b8ff748",  // Private key for account 6
-        "0x14dc79964da2c08b23698b3d3cc7ca32193d9955",  // Private key for account 7
-        "0x23618e81e3a08838ef12e834dffe61ca1d78e2b8",  // Private key for account 8
-        "0xa2386e1f9b1962c85e6a35c24dc4b1e25454278c",  // Private key for account 9
-        "0xa10d6a5029055a085e3f94bb9d8f94bf7f87c0e5",  // Private key for account 10
-        "0xe56e81a0492f17b580190b8b273f5a6374da4774",  // Private key for account 11
-        "0x74de5d4fcbf63e00296fd95d33236b9794016631",  // Private key for account 12
-        "0xbe2a26889d2f3ed6857270354f0c20c80b1ed4a7",  // Private key for account 13
-        "0xb5459d8e67917f5464f79d6a5a398047f2949ac1",  // Private key for account 14
-        "0x28e67e0d6f7dcd0b5b40edcd6749acba1534f04c",  // Private key for account 15
-        "0x324e6dcf3915a85f3c80f801d314615bcd3a3b8c",  // Private key for account 16
-        "0x50f2e8d2bdfdf18e1ab8de7064ea7ba1f1cbe8e2",  // Private key for account 17
-        "0xb4f30ce6c1adff9ee20541cb862c1a341f35d21e",  // Private key for account 18
-        "0x2c57a9f71db06cd8232f70b83df8e359ee7e08c5",  // Private key for account 19
-        "0xd42707c3ff75ce4fc9df749318c60b5e1e5df10a"   // Private key for account 20
-      ]
+      accounts: accounts
     }
   }
 };
-
-// export default config;
